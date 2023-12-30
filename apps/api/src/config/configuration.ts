716203ef836e087c7102f9eb493d145ded4fb4c2
lukaszzm <lukasz.malucha1@gmail.com>
2023-12-30 11:57:54 +0100

    feat(api): Add prisma and db

--- apps/api/src/config/configuration.ts
+++ apps/api/src/config/configuration.ts
@@ -2,8 +2,12 @@ import Joi from "joi";
 
 export default () => ({
   port: process.env.PORT,
+  database: {
+    url: process.env.DATABASE_URL,
+  },
 });
 
 export const configSchema = Joi.object({
   PORT: Joi.number().default(4000),
+  DATABASE_URL: Joi.string().required(),
 });
